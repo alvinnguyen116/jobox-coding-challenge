7d117b796f627b30b7abe55b9a1fa90de74dd0bb
alvin <@>
2020-04-03 17:23:18 -0700

    Updates

---
+++ precache-manifest.b16f249172f384097f071521c672f539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d938bd95865a1180e3c49eb427dba0",
+    "url": "/jobox-coding-challenge/index.html"
+  },
+  {
+    "revision": "c25bedf4c48e1be0ed73",
+    "url": "/jobox-coding-challenge/static/css/2.274a6d8f.chunk.css"
+  },
+  {
+    "revision": "326361c367b8efd5397a",
+    "url": "/jobox-coding-challenge/static/css/main.2972e453.chunk.css"
+  },
+  {
+    "revision": "c25bedf4c48e1be0ed73",
+    "url": "/jobox-coding-challenge/static/js/2.c789c53f.chunk.js"
+  },
+  {
+    "revision": "326361c367b8efd5397a",
+    "url": "/jobox-coding-challenge/static/js/main.2a8cf9d7.chunk.js"
+  },
+  {
+    "revision": "1a6a631b09624d47481e",
+    "url": "/jobox-coding-challenge/static/js/runtime-main.7afc2dcd.js"
+  }
+]);
